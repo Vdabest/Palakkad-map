72e72a1960b790c376f3f8ccb6c49fa0b34cda6d
Vdabest <varadhahari8@gmail.com>
2025-02-27 06:50:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Add markers for locations
 var locations = [
-    {name: "Palakkad Fort", lat: 10.7868, lon: 76.6542},
+    {name: "Palakkad Fort", description:"Palakkad Fort was constructed in 1766 AD, but little is known of its early history. The fort was seized and recaptured many times by past and previous rulers, but its beauty has remained the same. It is located at the heart of the town.",lat: 10.7868, lon: 76.6542},
     {name: "Malampuzha Dam", lat: 10.7768, lon: 76.6422},
     {name: "Silent Valley National Park", lat: 11.1235, lon: 76.4461},
     {name: "Dhoni Waterfalls", lat: 10.8212, lon: 76.5812}
